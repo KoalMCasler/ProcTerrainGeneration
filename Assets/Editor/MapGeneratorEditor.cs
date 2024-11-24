dc4f6749a912d2ba194175d92ecc48fa831c89a9
KoalMCasler <Theeoblivion@gmail.com>
2024-11-24 15:01:09 -0400

    Cleaned code

--- Assets/Editor/MapGeneratorEditor.cs
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -3,18 +3,22 @@ using System.Collections;
 using UnityEditor;
 
 [CustomEditor (typeof (MapGenerator))]
-public class MapGeneratorEditor : Editor {
+public class MapGeneratorEditor : Editor 
+{
 
-	public override void OnInspectorGUI() {
+	public override void OnInspectorGUI() 
+	{
 		MapGenerator mapGen = (MapGenerator)target;
 
-		if (DrawDefaultInspector ()) {
+		if (DrawDefaultInspector ()) 
+		{
 			if (mapGen.autoUpdate) {
 				mapGen.GenerateMap ();
 			}
 		}
 
-		if (GUILayout.Button ("Generate")) {
+		if (GUILayout.Button ("Generate")) 
+		{
 			mapGen.GenerateMap ();
 		}
 	}
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,36 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MapGenerator : MonoBehaviour {
+public class MapGenerator : MonoBehaviour 
+{
 
 	public enum DrawMode {NoiseMap, ColourMap, Mesh};
 	public DrawMode drawMode;
 
+
 	public int mapWidth;
 	public int mapHeight;
+	[Range(0.0001f,100)]
 	public float noiseScale;
-
+	[Range(1,10)]
 	public int octaves;
 	[Range(0,1)]
 	public float persistance;
+	[Range(1,10)]
 	public float lacunarity;
 
 	public int seed;
 	public Vector2 offset;
 
+	public bool useRandomSeed;
 	public bool autoUpdate;
 
 	public TerrainType[] regions;
 
-	public void GenerateMap() {
+	public void GenerateMap()
+	{
+		if(useRandomSeed)
+		{
+			seed = Random.Range(1,100000000);
+		}
 		float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
 		Color[] colourMap = new Color[mapWidth * mapHeight];
-		for (int y = 0; y < mapHeight; y++) {
-			for (int x = 0; x < mapWidth; x++) {
+		for (int y = 0; y < mapHeight; y++) 
+		{
+			for (int x = 0; x < mapWidth; x++) 
+			{
 				float currentHeight = noiseMap [x, y];
-				for (int i = 0; i < regions.Length; i++) {
-					if (currentHeight <= regions [i].height) {
+				for (int i = 0; i < regions.Length; i++) 
+				{
+					if (currentHeight <= regions [i].height) 
+					{
 						colourMap [y * mapWidth + x] = regions [i].colour;
 						break;
 					}
@@ -39,33 +53,24 @@ public class MapGenerator : MonoBehaviour {
 		}
 
 		MapDisplay display = FindObjectOfType<MapDisplay> ();
-		if (drawMode == DrawMode.NoiseMap) {
+		if (drawMode == DrawMode.NoiseMap) 
+		{
 			display.DrawTexture (TextureGenerator.TextureFromHeightMap (noiseMap));
-		} else if (drawMode == DrawMode.ColourMap) {
+		} 
+		else if (drawMode == DrawMode.ColourMap) 
+		{
 			display.DrawTexture (TextureGenerator.TextureFromColourMap (colourMap, mapWidth, mapHeight));
-		} else if (drawMode == DrawMode.Mesh) {
+		} 
+		else if (drawMode == DrawMode.Mesh) 
+		{
 			display.DrawMesh (MeshGenerator.GenerateTerrainMesh (noiseMap), TextureGenerator.TextureFromColourMap (colourMap, mapWidth, mapHeight));
-		}
-	}
-
-	void OnValidate() {
-		if (mapWidth < 1) {
-			mapWidth = 1;
-		}
-		if (mapHeight < 1) {
-			mapHeight = 1;
-		}
-		if (lacunarity < 1) {
-			lacunarity = 1;
-		}
-		if (octaves < 0) {
-			octaves = 0;
 		}
 	}
 }
 
 [System.Serializable]
-public struct TerrainType {
+public struct TerrainType 
+{
 	public string name;
 	public float height;
 	public Color colour;
